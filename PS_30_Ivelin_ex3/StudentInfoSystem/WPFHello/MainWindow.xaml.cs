66fc6a00c570c769b5121846d9ebe6fe6ac1a240
IvelinPetkov30 <103790820+IvelinPetkov30@users.noreply.github.com>
2022-04-19 00:09:58 +0300

    Add files via upload

---
+++ PS_30_Ivelin_ex3/StudentInfoSystem/WPFHello/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            foreach (var item in gridgrid.Children)
+            {
+                if (item is TextBox)
+                {
+                    s = s + ((TextBox)item).Text;
+                    s = s + '\n';
+                }
+            }
+            if (txtName.Text.Length >= 2)
+            {
+                MessageBox.Show("Hello, " + s + "\nThis is my first program in VS 2022.");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid name, with 2 characters or more.");
+            }
+
+        }
+
+        public void Window_Close(object sender, ConsoleCancelEventArgs e)
+        {
+            string msg = "Close without saving?";
+            MessageBoxResult result =
+              MessageBox.Show(
+                msg,
+                "Data App",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+
+        }
+    }
+}
