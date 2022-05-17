1dca391c31d614411abb87b9c3e4c5603d2e532e
IvelinPetkov30 <103790820+IvelinPetkov30@users.noreply.github.com>
2022-05-18 01:09:39 +0300

    Add files via upload

---
+++ PS_30_ALL/WpfExample/InfoCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfExample
+{
+    public class InfoCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Hello, world!");
+            NamesWindow names = new NamesWindow();
+            names.Show();
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        private InfoCommand _infoCommand = new InfoCommand();
+        public InfoCommand InformationCommand
+        {
+            get { return _infoCommand; }
+        }
+        
+    }
+}
